a63742b8ba41ad3fb31ccf191d94141f3f1b8f47
Lunacywonders <Isaacn9394@gmail.com>
2025-10-08 09:32:33 -0700

    afds

---
+++ index.js
@@ -0,0 +1,208 @@
+const panda = [
+  {
+    name: "Panda",
+    displayName: "Panda",
+    price: 1200,
+    age: 13,
+    sibling: "Curious Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.-EV5VFhUSmW6IV75xavQnQHaE8?w=274&h=182&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Curious Panda",
+    displayName: "Curious Panda",
+    price: 1500,
+    age: 10,
+    sibling: "Sleepy Panda",
+    imgSrc: "https://sdzwildlifeexplorers.org/sites/default/files/2017-07/pandas-closeup.jpg",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Sleepy Panda",
+    price: 1000,
+    age: 8,
+    sibling: "Playful Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.DV83rMEREmos9D4zF6350QHaFj?w=238&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Playful Panda",
+    price: 1800,
+    age: 7,
+    sibling: "Happy Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.UI_sehNo0FRV3XIXccQ1PgHaE7?w=276&h=184&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Happy Panda",
+    price: 2000,
+    age: 5,
+    sibling: "Grumpy Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.nuZ-ve8COpCGAAYFmjrRmgHaEK?w=328&h=184&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Grumpy Panda",
+    price: 900,
+    age: 4,
+    sibling: "Lazy Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.e5jdPm02mk2b5kwfBNzaYQHaFl?w=238&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Lazy Panda",
+    price: 850,
+    age: 6,
+    sibling: "Jumpy Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.pyPHFdqAzKCGdOXxi9DvHwHaEK?w=272&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Jumpy Panda",
+    price: 1300,
+    age: 3,
+    sibling: "Sneaky Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.-INJNxW1OSx7V_7gmL4IogHaFj?w=229&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Sneaky Panda",
+    price: 1100,
+    age: 2,
+    sibling: "Brave Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.Rzr9BNSavsbq-nKcdRxVnwHaE8?w=257&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Brave Panda",
+    price: 2500,
+    age: 9,
+    sibling: "Shy Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.iiB7l9_kVoUfi0EfjH01hgHaE8?w=278&h=185&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Shy Panda",
+    price: 950,
+    age: 1,
+    sibling: "Strong Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.qDgfSv6qWlXxXWYa1XrCXAHaE7?w=279&h=185&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Strong Panda",
+    price: 2700,
+    age: 12,
+    sibling: "Kind Panda",
+    imgSrc: "https://th.bing.com/th/id/OIP.SGBfCkoiFtsSiQbO25Z2XgHaEK?w=333&h=187&c=7&r=0&o=7&cb=12&pid=1.7&rm=3",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Kind Panda",
+    price: 1800,
+    age: 5,
+    sibling: "Smart Panda"
+    imgSrc: "https://th.bing.com/th/id/OIP.Pixq9eppCBGpifpmTPXgFwHaE7?w=258&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Smart Panda",
+    price: 2300,
+    age: 11,
+    sibling: "Bold Panda"
+    imgSrc: "https://tse3.mm.bing.net/th/id/OIP.ZyPsg0F10rhgwT2mi34vbQHaE8?cb=12&pid=ImgDet&w=203&h=135&c=7&o=7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Bold Panda",
+    price: 3200,
+    age: 8,
+    sibling: "Funny Panda"
+    imgSrc: "https://tse1.mm.bing.net/th/id/OIP.SzLqtpd2PPSCvTW73Ly_jQHaHa?cb=12&rs=1&pid=ImgDetMain&o=7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Funny Panda",
+    price: 1500,
+    age: 7,
+    sibling: "Gentle Panda"
+    imgSrc: "https://tse2.mm.bing.net/th/id/OIP.1s1OhHYzIPfIBFfiTGVaXgHaFk?cb=12&pid=ImgDet&w=203&h=152&c=7&o=7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Gentle Panda",
+    price: 1400,
+    age: 4,
+    sibling: "Wild Panda"
+    imgSrc: "https://i.natgeofe.com/k/6a96c3b4-2be1-498f-972e-76a20d973376/3_Baby_Pandas_KIDS_0322_16x9.jpg"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Wild Panda",
+    price: 2000,
+    age: 6,
+    sibling: "Friendly Panda"
+    imgSrc: "https://tse4.mm.bing.net/th/id/OIP.Po6RvZ40sp5HTEM_oCg98gHaE6?cb=12&w=1400&h=930&rs=1&pid=ImgDetMain&o=7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Friendly Panda",
+    price: 1700,
+    age: 3,
+    sibling: "Loyal Panda"
+    imgSrc: "https://th.bing.com/th/id/OIP.6sGw33bSNgUBGVny-UwswgHaEc?w=298&h=180&c=7&r=0&o=7&cb=12&pid=1.7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Loyal Panda",
+    price: 2900,
+    age: 10,
+    sibling: "Quiet Panda"
+    imgSrc: "https://th.bing.com/th/id/OIP.1eHVODKu68Vt9vwuxPjvngHaE7?w=282&h=188&c=7&r=0&o=7&cb=12&pid=1.7&rm=3"
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Baby Panda",
+    displayName: "Baby Panda",
+    price: 1,
+    imgSrc: "https://people.com/thmb/Vd9NrelKIcK3o0HedDOLvET3qYw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(671x634:673x636)/panda-9a0ef8a8f9314d3c908ce62e94a644a7.jpg",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "AI Panda",
+    displayName: "AI Panda",
+    price: 1,
+    imgSrc: "https://s.wsj.net/public/resources/images/BN-YC933_PANDAS_GR_20180404175718.jpg",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Panda eating Stick",
+    displayName: "Panda eating Stick",
+    price: 1,
+    imgSrc: "https://images.seattletimes.com/wp-content/uploads/2021/02/panda-050c061c-60de-11eb-9061-07abcc1f9229.jpg?d=780x520",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Panda eating Pollen",
+    displayName: "Panda eating Pollen",
+    price: 1,
+    imgSrc: "https://dcist.com/wp-content/uploads/sites/3/2021/01/20210121-xiao-qi-ji-potato-lauriethompson005-900x600.jpg",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Red panda",
+    displayName: "Red panda",
+    price: 1,
+    imgSrc: "https://s7d2.scene7.com/is/image/TWCNews/red_panda_tongue_ky_0516",
+    link: "https://www.pandaexpress.com/"
+  },
+  {
+    name: "Chinese Panda",
+    displayName: "Chinese Panda",
+    price: 1,
+    imgSrc: "https://cdn-media.kontinentalist.com/panda-diplomacy-gifting-pandas-to-further-chinas-politics/panda-diplomacy-gifting-pandas-to-further-chinas-politics.jpg",
+    link: "https://www.pandaexpress.com/"
+  }
+];
+panda.forEach((panda) => makeCard(panda));
+function makeCard(item) {}
